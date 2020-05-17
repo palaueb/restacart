13e35023e6203b6641756dcab324378b716a050f
palaueb <marc@palaueb.com>
2020-05-17 22:26:15 +0200

    added the date of the upload and some stetic improvements

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ Class RestaCart {
 html{font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}body{margin:0}article,aside,details,figcaption,figure,footer,header,hgroup,main,menu,nav,section,summary{display:block}audio,canvas,progress,video{display:inline-block;vertical-align:baseline}audio:not([controls]){display:none;height:0}[hidden],template{display:none}a{background-color:transparent}a:active,a:hover{outline:0}abbr[title]{border-bottom:1px dotted}b,strong{font-weight:700}dfn{font-style:italic}h1{font-size:2em;margin:.67em 0}mark{background:#ff0;color:#000}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sup{top:-.5em}sub{bottom:-.25em}img{border:0}svg:not(:root){overflow:hidden}figure{margin:1em 40px}hr{-moz-box-sizing:content-box;box-sizing:content-box;height:0}pre{overflow:auto}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em}button,input,optgroup,select,textarea{color:inherit;font:inherit;margin:0}button{overflow:visible}button,select{text-transform:none}button,html input[type="button"],/* 1 */
 input[type="reset"],input[type="submit"]{-webkit-appearance:button;cursor:pointer}button[disabled],html input[disabled]{cursor:default}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}input{line-height:normal}input[type="checkbox"],input[type="radio"]{box-sizing:border-box;padding:0}input[type="number"]::-webkit-inner-spin-button,input[type="number"]::-webkit-outer-spin-button{height:auto}input[type="search"]{-webkit-appearance:textfield;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box}input[type="search"]::-webkit-search-cancel-button,input[type="search"]::-webkit-search-decoration{-webkit-appearance:none}fieldset{border:1px solid silver;margin:0 2px;padding:.35em .625em .75em}legend{border:0;padding:0}textarea{overflow:auto}optgroup{font-weight:700}table{border-collapse:collapse;border-spacing:0}td,th{padding:0}
 .container{position:relative;width:100%;max-width:960px;margin:0 auto;padding:0 20px;box-sizing:border-box}.column,.columns{width:100%;float:left;box-sizing:border-box}@media (min-width: 400px){.container{width:85%;padding:0}}@media (min-width: 550px){.container{width:80%}.column,.columns{margin-left:4%}.column:first-child,.columns:first-child{margin-left:0}.one.column,.one.columns{width:4.66666666667%}.two.columns{width:13.3333333333%}.three.columns{width:22%}.four.columns{width:30.6666666667%}.five.columns{width:39.3333333333%}.six.columns{width:48%}.seven.columns{width:56.6666666667%}.eight.columns{width:65.3333333333%}.nine.columns{width:74%}.ten.columns{width:82.6666666667%}.eleven.columns{width:91.3333333333%}.twelve.columns{width:100%;margin-left:0}.one-third.column{width:30.6666666667%}.two-thirds.column{width:65.3333333333%}.one-half.column{width:48%}.offset-by-one.column,.offset-by-one.columns{margin-left:8.66666666667%}.offset-by-two.column,.offset-by-two.columns{margin-left:17.3333333333%}.offset-by-three.column,.offset-by-three.columns{margin-left:26%}.offset-by-four.column,.offset-by-four.columns{margin-left:34.6666666667%}.offset-by-five.column,.offset-by-five.columns{margin-left:43.3333333333%}.offset-by-six.column,.offset-by-six.columns{margin-left:52%}.offset-by-seven.column,.offset-by-seven.columns{margin-left:60.6666666667%}.offset-by-eight.column,.offset-by-eight.columns{margin-left:69.3333333333%}.offset-by-nine.column,.offset-by-nine.columns{margin-left:78%}.offset-by-ten.column,.offset-by-ten.columns{margin-left:86.6666666667%}.offset-by-eleven.column,.offset-by-eleven.columns{margin-left:95.3333333333%}.offset-by-one-third.column,.offset-by-one-third.columns{margin-left:34.6666666667%}.offset-by-two-thirds.column,.offset-by-two-thirds.columns{margin-left:69.3333333333%}.offset-by-one-half.column,.offset-by-one-half.columns{margin-left:52%}}html{font-size:62.5%}body{font-size:1.5em;line-height:1.6;font-weight:400;font-family:"Raleway","HelveticaNeue","Helvetica Neue",Helvetica,Arial,sans-serif;color:#222}h1,h2,h3,h4,h5,h6{margin-top:0;margin-bottom:2rem;font-weight:300}h1{font-size:4rem;line-height:1.2;letter-spacing:-.1rem}h2{font-size:3.6rem;line-height:1.25;letter-spacing:-.1rem}h3{font-size:3rem;line-height:1.3;letter-spacing:-.1rem}h4{font-size:2.4rem;line-height:1.35;letter-spacing:-.08rem}h5{font-size:1.8rem;line-height:1.5;letter-spacing:-.05rem}h6{font-size:1.5rem;line-height:1.6;letter-spacing:0}@media (min-width: 550px){h1{font-size:5rem}h2{font-size:4.2rem}h3{font-size:3.6rem}h4{font-size:3rem}h5{font-size:2.4rem}h6{font-size:1.5rem}}p{margin-top:0}a{color:#1EAEDB}a:hover{color:#0FA0CE}.button,button,input[type="submit"],input[type="reset"],input[type="button"]{display:inline-block;height:38px;padding:0 30px;color:#555;text-align:center;font-size:11px;font-weight:600;line-height:38px;letter-spacing:.1rem;text-transform:uppercase;text-decoration:none;white-space:nowrap;background-color:transparent;border-radius:4px;border:1px solid #bbb;cursor:pointer;box-sizing:border-box}.button:hover,button:hover,input[type="submit"]:hover,input[type="reset"]:hover,input[type="button"]:hover,.button:focus,button:focus,input[type="submit"]:focus,input[type="reset"]:focus,input[type="button"]:focus{color:#333;border-color:#888;outline:0}.button.button-primary,button.button-primary,input[type="submit"].button-primary,input[type="reset"].button-primary,input[type="button"].button-primary{color:#FFF;background-color:#33C3F0;border-color:#33C3F0}.button.button-primary:hover,button.button-primary:hover,input[type="submit"].button-primary:hover,input[type="reset"].button-primary:hover,input[type="button"].button-primary:hover,.button.button-primary:focus,button.button-primary:focus,input[type="submit"].button-primary:focus,input[type="reset"].button-primary:focus,input[type="button"].button-primary:focus{color:#FFF;background-color:#1EAEDB;border-color:#1EAEDB}input[type="email"],input[type="number"],input[type="search"],input[type="text"],input[type="tel"],input[type="url"],input[type="password"],textarea,select{height:38px;padding:6px 10px;background-color:#fff;border:1px solid #D1D1D1;border-radius:4px;box-shadow:none;box-sizing:border-box}input[type="email"],input[type="number"],input[type="search"],input[type="text"],input[type="tel"],input[type="url"],input[type="password"],textarea{-webkit-appearance:none;-moz-appearance:none;appearance:none}textarea{min-height:65px;padding-top:6px;padding-bottom:6px}input[type="email"]:focus,input[type="number"]:focus,input[type="search"]:focus,input[type="text"]:focus,input[type="tel"]:focus,input[type="url"]:focus,input[type="password"]:focus,textarea:focus,select:focus{border:1px solid #33C3F0;outline:0}label,legend{display:block;margin-bottom:.5rem;font-weight:600}fieldset{padding:0;border-width:0}input[type="checkbox"],input[type="radio"]{display:inline}label > .label-body{display:inline-block;margin-left:.5rem;font-weight:400}ul{list-style:circle inside}ol{list-style:decimal inside}ol,ul{padding-left:0;margin-top:0}ul ul,ul ol,ol ol,ol ul{margin:1.5rem 0 1.5rem 3rem;font-size:90%}li{margin-bottom:1rem}code{padding:.2rem .5rem;margin:0 .2rem;font-size:90%;white-space:nowrap;background:#F1F1F1;border:1px solid #E1E1E1;border-radius:4px}pre > code{display:block;padding:1rem 1.5rem;white-space:pre}th,td{padding:12px 15px;text-align:left;border-bottom:1px solid #E1E1E1}th:first-child,td:first-child{padding-left:0}th:last-child,td:last-child{padding-right:0}button,.button{margin-bottom:1rem}input,textarea,select,fieldset{margin-bottom:1.5rem}pre,blockquote,dl,figure,table,p,ul,ol,form{margin-bottom:1.5rem}.u-full-width{width:100%;box-sizing:border-box}.u-max-full-width{max-width:100%;box-sizing:border-box}.u-pull-right{float:right}.u-pull-left{float:left}hr{margin-top:3rem;margin-bottom:3.5rem;border-width:0;border-top:1px solid #E1E1E1}.container:after,.row:after,.u-cf{content:"";display:table;clear:both}
-.container ul.row li{list-style-type: none;}
+ul,li{list-style-type: none;}
 .alert{
     border:0.2em solid #d77c7c;
     border-radius:2em;
@@ -311,7 +311,7 @@ UPLOADFORM;
             
             $current_path = $_SERVER['SCRIPT_URI'];
             $url = $current_path.$destination;
-            $qr_path = $this->config['file_folder'].'qr_'.round(rand()*100000).'_'.$basic_time.'.png';
+            $qr_path = $this->config['file_folder'].'qr_'.round(rand(0,100000)).'_'.$basic_time.'.png';
             if(!$this->create_qr($url, $qr_path)){
                 $this->set_error('El QR no se ha podido generar correctamente.');
             }
@@ -321,15 +321,12 @@ UPLOADFORM;
                 'original_name'=>$upload_file["name"],
                 'menu_url'=>$url,
                 'qr_path'=>$qr_path,
-                'label'=>$label
-                //here goes more future information
+                'label'=>$label,
+                'date'=>date('d/m/Y',$basic_time)
             ];
             //TODO: move this to his own folder of configs
             file_put_contents('.info_'.$basic_time.'.json', json_encode($current_data));
-            die($url);
         }
-
-
     }
     private function logout(){
         session_start();
@@ -414,12 +411,23 @@ UPLOADFORM;
         $this->add_text('TITLE_PAGE','Configuración de RestaCart.');
         
         $this->add_content("<h4>Listado de cartas con código QR</h4>");
-        $elements_list = "<div class='container'><ul class='row'>";
+        $elements_list = "<div><ul>";
         
         $config_files = $this->get_config_files();
         foreach($config_files as $config_file){
             $data_menu = $this->load_config_file($config_file);
-            $elements_list .= "<li class='twelve columns'><div>JUST A LIST OF ROWS</div></li>";
+            $elements_list .= "<li><div class='container'>";
+            
+            $elements_list .= "<div class='row'>";
+            $elements_list .= " <div class='eight columns'>";
+            $elements_list .= "     <h5>".$data_menu['label']."</h5>"; //name in title
+            $elements_list .= "     <p>Carta añadida el dia ".$data_menu['date']."</p>";
+            $elements_list .= "     <p><a href='".$data_menu['menu_url']."'>Descargar el fichero que visualizan los clientes: ".$data_menu['original_name']."</a></p>";
+            $elements_list .= " </div>";
+            $elements_list .= " <div class='four columns'><a href='".$data_menu['qr_path']."'><img src='".$data_menu['qr_path']."' style='width:100%' /></a></div>";
+            $elements_list .= "</div>";
+            
+            $elements_list .="</div></li>";
         }
         
         $elements_list .= "</ul></div>";
@@ -505,33 +513,33 @@ UPLOADFORM;
         // TODO: return the right error message in case it fails
         return true;
     }
-    public function print_qr(){
-/*
-s - Symbology (type of QR code). One of:
-    qr
-    qr-l <- poca redundància de dades
-    qr-m <- mitja redundància de dades
-    qr-q <- alta redundància de dades
-    qr-h <- molt alta redundància de dades
-d - Data. Encode in Shift-JIS for kanji mode.
-w - Width of image. Overrides sf or sx.
-h - Height of image. Overrides sf or sy.
-sf - Scale factor. Default is 4.
-sx - Horizontal scale factor. Overrides sf.
-sy - Vertical scale factor. Overrides sf.
-p - Padding. Default is 0.
-pv - Top and bottom padding. Default is value of p.
-ph - Left and right padding. Default is value of p.
-pt - Top padding. Default is value of pv.
-pl - Left padding. Default is value of ph.
-pr - Right padding. Default is value of ph.
-pb - Bottom padding. Default is value of pv.
-bc - Background color in #RRGGBB format.
-fc - Foreground color in #RRGGBB format.
-md - Module density. A number between 0 and 1. Default is 1.
-wq - Width of quiet area units. Default is 1. Use 0 to suppress quiet area.
-wm - Width of narrow modules and spaces. Default is 1.
-*/
+    public function print_qr(){ //unused
+        /*
+        s - Symbology (type of QR code). One of:
+            qr
+            qr-l <- poca redundància de dades
+            qr-m <- mitja redundància de dades
+            qr-q <- alta redundància de dades
+            qr-h <- molt alta redundància de dades
+        d - Data. Encode in Shift-JIS for kanji mode.
+        w - Width of image. Overrides sf or sx.
+        h - Height of image. Overrides sf or sy.
+        sf - Scale factor. Default is 4.
+        sx - Horizontal scale factor. Overrides sf.
+        sy - Vertical scale factor. Overrides sf.
+        p - Padding. Default is 0.
+        pv - Top and bottom padding. Default is value of p.
+        ph - Left and right padding. Default is value of p.
+        pt - Top padding. Default is value of pv.
+        pl - Left padding. Default is value of ph.
+        pr - Right padding. Default is value of ph.
+        pb - Bottom padding. Default is value of pv.
+        bc - Background color in #RRGGBB format.
+        fc - Foreground color in #RRGGBB format.
+        md - Module density. A number between 0 and 1. Default is 1.
+        wq - Width of quiet area units. Default is 1. Use 0 to suppress quiet area.
+        wm - Width of narrow modules and spaces. Default is 1.
+        */
         $options = ["s"=>"qr-q"];
         $generator = new QRCode('https://www.github.com/palaueb/restacart/', $options);
         $generator->output_image();
